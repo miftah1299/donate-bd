ddd83a47a57631cfd08fadae966ec030059810c8
Your Name <jannat27120@gmail.com>
2024-09-25 03:15:43 +0600

    Refactor code to show/hide sections based on button clicks

---
+++ js/features.js
@@ -0,0 +1,23 @@
+// toggle history button
+document
+    .getElementById("btn-toggle-history")
+    .addEventListener("click", function () {
+        showSectionById("history");
+        // change color of button
+        document.getElementById("btn-toggle-history").classList.add("bg-primary");
+        document.getElementById("btn-toggle-donation").classList.remove("bg-primary");
+        document.getElementById("btn-toggle-donation").classList.add("btn-outline");
+    });
+
+// toggle donation button
+document
+    .getElementById("btn-toggle-donation")
+    .addEventListener("click", function () {
+        showSectionById("donation");
+
+        // change color of button
+        document.getElementById("btn-toggle-donation").classList.remove("btn-outline");
+        document.getElementById("btn-toggle-donation").classList.add("bg-primary");
+        document.getElementById("btn-toggle-history").classList.remove("bg-primary");
+        document.getElementById("btn-toggle-history").classList.add("btn-outline");
+    });
---
+++ js/utilities.js
@@ -0,0 +1,10 @@
+// show section by id
+function showSectionById(id) {
+    document.getElementById("history").classList.add("hidden");
+    document.getElementById("donation").classList.add("hidden");
+
+    // show section
+    document.getElementById(id).classList.remove("hidden");
+
+    // show color
+}
