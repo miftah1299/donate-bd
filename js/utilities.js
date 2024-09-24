4d9b27fad3aa3b5366d8798df97c272fb4ed5fa0
miftah1299 <jannat27120@gmail.com>
2024-09-25 03:42:56 +0600

    Calculated  donation using js functions

--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,20 @@
+// common functions for reusability
+// for input fields
+function getInputFieldById(id) {
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    console.log(id, inputNumber);
+    return inputNumber;
+}
+
+// for text fields
+function getTextFieldById(id) {
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    console.log(id, textNumber);
+    return textNumber;
+}
+
 // show section by id
 function showSectionById(id) {
     document.getElementById("history").classList.add("hidden");
