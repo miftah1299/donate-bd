4b1e6fc39d05f227f44411853b4b1e064251b714
Your Name <jannat27120@gmail.com>
2024-09-25 12:32:05 +0600

    Changed navbar style when scrolling

--- js/features.js
+++ js/features.js
@@ -4,9 +4,15 @@ document
     .addEventListener("click", function () {
         showSectionById("history");
         // change color of button
-        document.getElementById("btn-toggle-history").classList.add("bg-primary");
-        document.getElementById("btn-toggle-donation").classList.remove("bg-primary");
-        document.getElementById("btn-toggle-donation").classList.add("btn-outline");
+        document
+            .getElementById("btn-toggle-history")
+            .classList.add("bg-primary");
+        document
+            .getElementById("btn-toggle-donation")
+            .classList.remove("bg-primary");
+        document
+            .getElementById("btn-toggle-donation")
+            .classList.add("btn-outline");
     });
 
 // toggle donation button
@@ -16,8 +22,26 @@ document
         showSectionById("donation");
 
         // change color of button
-        document.getElementById("btn-toggle-donation").classList.remove("btn-outline");
-        document.getElementById("btn-toggle-donation").classList.add("bg-primary");
-        document.getElementById("btn-toggle-history").classList.remove("bg-primary");
-        document.getElementById("btn-toggle-history").classList.add("btn-outline");
+        document
+            .getElementById("btn-toggle-donation")
+            .classList.remove("btn-outline");
+        document
+            .getElementById("btn-toggle-donation")
+            .classList.add("bg-primary");
+        document
+            .getElementById("btn-toggle-history")
+            .classList.remove("bg-primary");
+        document
+            .getElementById("btn-toggle-history")
+            .classList.add("btn-outline");
     });
+
+// navbar scroll
+window.addEventListener("scroll", function () {
+    const header = document.getElementById("header");
+    if (window.scrollY > 0) {
+        header.classList.add("bg-opacity-30");
+    } else {
+        header.classList.remove("bg-opacity-30");
+    }
+});
